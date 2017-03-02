618e1c8a9bd20b8742d1251627300fcbb03ba958
Ojas Velamkar <ojasvelamkar@Ojass-MBP.gateway>
2017-03-02 21:19:26 +1100

    added hbs view etc

--- server.js
+++ server.js
@@ -1,18 +1,51 @@
 var express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 const port = process.env.PORT || 3000;
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello express</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.send('Help page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
